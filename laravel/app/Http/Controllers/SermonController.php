ee479b872eb413b389ca9d4882a00a100c66b321
Chea <chornchhayvanichea@gmail.com>
2026-02-02 22:08:46 +0700

    added crud to Sermon controller

--- laravel/app/Http/Controllers/SermonController.php
+++ laravel/app/Http/Controllers/SermonController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SermonRequest;
+use App\Http\Requests\Sermon\SermonStoreRequest;
+use App\Http\Requests\Sermon\SermonUpdateRequest;
+use App\Http\Resources\SermonResource;
+use App\Models\Sermon;
+use App\Services\FileHandling;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class SermonController extends Controller
 {
-    public function store(SermonRequest $sermonRequest) {}
+    private const SERMON_DIR = 'Sermon/thumbnail';
+
+    public function show(Sermon $sermon): SermonResource
+    {
+        return new SermonResource($sermon);
+    }
+
+    public function index(): AnonymousResourceCollection
+    {
+        $sermons = Sermon::with('series')->paginate(15);
+
+        return SermonResource::collection($sermons);
+    }
+
+    public function store(SermonStoreRequest $request, FileHandling $fileHandling): SermonResource
+    {
+        $validated = $request->validated();
+        $thumbnailPath = $fileHandling->uploadFile($request->file('thumbnail'), self::SERMON_DIR);
+        if ($thumbnailPath) {
+            $validated['thumbnail'] = $thumbnailPath;
+        }
+        $sermon = Sermon::create($validated);
+        $sermon->load(['series', 'users']);
+
+        return new SermonResource($sermon);
+    }
+
+    public function update(SermonUpdateRequest $request, Sermon $sermon, FileHandling $fileHandling): SermonResource
+    {
+
+        $validated = $request->validated();
+        $thumbnailPath = $fileHandling->uploadFile($request->file('thumbnail'), self::SERMON_DIR);
+        if ($thumbnailPath) {
+            $fileHandling->deleteFile($sermon->thumbnail);
+            $validated['thumbnail'] = $thumbnailPath;
+        }
+        $sermon->update($validated);
+
+        return new SermonResource($sermon);
+    }
+
+    public function destroy(Sermon $sermon): JsonResponse
+    {
+        $sermon->delete();
+
+        return response()->json([
+            'message' => 'sermon has been deleted',
+            'success' => true,
+        ]);
+    }
 }
